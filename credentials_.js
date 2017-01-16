a97589fc2f3dcce01a624fd58072fdee38edf2ec
luisjarq <lc.luisja@gmail.com>
2017-01-16 18:48:02 +0100

    Update credentials_.js

--- credentials_.js
+++ credentials_.js
@@ -20,8 +20,8 @@ var credentials ={
 	credentials: {
 		// Replace placeholder below by the Consumer Key and Consumer Secret you got from
 		// http://developer.autodesk.com/ for the production server
-		client_id: process.env.CONSUMERKEY || '<replace with your consumer key>',
-		client_secret: process.env.CONSUMERSECRET || '<replace with your consumer secret>',
+		client_id: process.env.CONSUMERKEY || '8lIcVTExetez4plY8kvptOeClQDz5Qgm',
+		client_secret: process.env.CONSUMERSECRET || 'Ec19b1c67a681418',
 		grant_type: 'client_credentials',
 		scope: 'data:read'
 	},
